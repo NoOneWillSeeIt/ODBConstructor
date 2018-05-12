b227cd58dbcb895fe7d7063bdaa2412a7a965f7a
NoOneWillSeeIt <rest338@gmail.com>
2018-05-12 23:01:22 +0800

    added some fancy css and improved onclick

--- scripts/hello.js
+++ scripts/hello.js
@@ -6,13 +6,13 @@ canvas.onclick = function(e) {
 	
 	var div = document.createElement('div');
 	div.className = "table-container";
-	div.style.width = 100;
-	div.style.height = 60;
+	// div.style.width = 100 + "px";
+	// div.style.height = 80 + "px";
 	var header = document.createElement('p');
 	header.innerHTML = "Table 1";
-	header.style.align = "center";
-	header.style.font_weight = "bold";
-	header.style.width = 100 + "%";
+	// header.style.alignSelf = "center";
+	// header.style.font_weight = "bold";
+	// header.style.width = 100 + "%";
 	div.appendChild(header);
 
 	var table = document.createElement('table');
@@ -26,7 +26,29 @@ canvas.onclick = function(e) {
 
 	div.style.left = obj.x + "px";
 	div.style.top = obj.y + "px";
+
+	console.log(obj);
+
 	canvas.appendChild(div);
+
+	var canvasCSS = window.getComputedStyle(canvas);
+	var Zayka = window.getComputedStyle(div);
+	alert("x = " + Zayka.width + " objx = " + obj.x + 
+		"y = " + Zayka.height + " objy = " + obj.y);
+
+	console.log(Zayka);
+
+	alert("x = " + (obj.x + parseInt(Zayka.width) > parseInt(canvasCSS.width)) +
+		" y = " + (obj.y + parseInt(Zayka.height) > parseInt(canvasCSS.height)));
+
+	if (obj.x + parseInt(Zayka.width) > parseInt(canvasCSS.width)) {
+		div.style.left = parseInt(canvasCSS.width) - parseInt(Zayka.width) - 
+			parseInt(Zayka.borderRight) - 10 + "px";
+	}
+	if (obj.y + parseInt(Zayka.height) > parseInt(canvasCSS.height)) {
+		div.style.top = parseInt(canvasCSS.height) - parseInt(Zayka.height) -
+			parseInt(Zayka.borderBottom) - 10 + "px";
+	}
 }
 
 function getOnCanvasClickCoords(clientX, clientY) {
