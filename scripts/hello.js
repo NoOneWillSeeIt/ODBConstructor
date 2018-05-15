6e5786e037f56c89f13ae30b3257370008a6fda5
NoOneWillSeeIt <rest338@gmail.com>
2018-05-15 22:07:40 +0800

    changed layout

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,54 +1,47 @@
 ﻿"use strict";
 var body = document.body;
 var canvas = document.getElementById("canvas");
-var canvasHolder = document.getElementById("canvas-holder");
 canvas.onclick = function(e) {
 	
 	//TODO: add drag-n-drop
 
-	var div = document.createElement('div');
-	div.className = "table-container";
-	var header = document.createElement('p');
-	header.innerHTML = "Table 1";
-	div.appendChild(header);
-
 	var table = document.createElement('table');
-	table.style.width = 100;
+	table.className = "table-container";
+	table.createTHead();
+	table.tHead.innerHTML = "Table 1";
+	
 	var initRow = table.insertRow(0);
 	initRow.insertCell(0).innerHTML = "Cell 1";
 	initRow.insertCell(1).innerHTML = "Cell 2";
-	div.appendChild(table);
 
 	var obj = getOnCanvasClickCoords(e.clientX, e.clientY);
 
-	div.style.left = obj.x + "px";
-	div.style.top = obj.y + "px";
+	table.style.left = obj.x + "px";
+	table.style.top = obj.y + "px";
 
-	canvas.appendChild(div);
+	canvas.appendChild(table);
 
 	var canvasCSS = window.getComputedStyle(canvas);
-	var Zayka = window.getComputedStyle(div);
+	var Zayka = window.getComputedStyle(table);
 
 	if (obj.x + parseInt(Zayka.width) > parseInt(canvasCSS.width)) {
-		div.style.left = parseInt(canvasCSS.width) - parseInt(Zayka.width) - 
+		table.style.left = parseInt(canvasCSS.width) - parseInt(Zayka.width) - 
 			parseInt(Zayka.borderRight) - 10 + "px";
 	}
 	if (obj.y + parseInt(Zayka.height) > parseInt(canvasCSS.height)) {
-		div.style.top = parseInt(canvasCSS.height) - parseInt(Zayka.height) -
+		table.style.top = parseInt(canvasCSS.height) - parseInt(Zayka.height) -
 			parseInt(Zayka.borderBottom) - 10 + "px";
 	}
 }
 
 function getOnCanvasClickCoords(clientX, clientY) {
-	var cHPos = canvasHolder.getBoundingClientRect();
-	var canvasCSS = window.getComputedStyle(canvas);
-	
-	var canvasCoords = {};
-	canvasCoords.x = clientX - cHPos.x - 
-		parseInt(canvasCSS.marginLeft) + canvasHolder.scrollLeft;
+	var canvasHolder = document.getElementById("canvas-holder");
+	var canvasHolderRect = canvasHolder.getBoundingClientRect();
 
-	canvasCoords.y = clientY - cHPos.y - 
-		parseInt(canvasCSS.marginTop) + canvasHolder.scrollTop;
+	var canvasCoords = {};
+	canvasCoords.x = clientX + canvasHolder.scrollLeft - canvasHolderRect.x;
+
+	canvasCoords.y = clientY + canvasHolder.scrollTop - canvasHolderRect.y;
 
 	return canvasCoords;
 }
