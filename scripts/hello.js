62d1d2fe4e5ee731e732d7cc11208443099aa071
NoOneWillSeeIt <rest338@gmail.com>
2018-06-10 21:40:44 +0800

    one step to make editable fields

--- scripts/hello.js
+++ scripts/hello.js
@@ -3,20 +3,77 @@ let body = document.body;
 let canvas = document.getElementById("canvas");
 let contextMenu = document.getElementById("canvas-context");
 
-function tableManager(table) {
-	let self = this;
-	this.table = table;
-	this.tbody = table.tBodies[0];
-	console.log(table, table.tBodies);
-	this.appendRow = function (){
-		console.log(table, table.tBodies);
-		let row = self.tbody.insertRow(0);
-		row.insertCell(0).innerHTML="asd";
-		row.insertCell(1).innerHTML="asdasd";
+function tableManager(table, tname) {
+	let name = tname;
+	this.DOMtable = table;
+	let tbody = this.DOMtable.tBodies[0];
+	function field(options) {
+		if (options.name == undefined || options.type == undefined) {
+			alert("Неверные значения имени и типа поля");
+			return null;
+		}
+		this.name = options.name;
+		this.type = options.type;
+		this.nullable = options.nullable || false;
+		this.primK = options.primK || false;
+		this.autoinc = options.autoinc || false;
+		this.typeOpt1 = options.typeOpt1 || null;
+		this.typeOpt2 = options.typeOpt2 || null;
+		this.FK = options.FK || null;
+		let self = this;
+		this.editField = function(options) {
+			self.name = options.name || self.name;
+			self.type = options.type || self.type;
+			self.nullable = options.nullable || self.nullable;
+			self.primK = options.primK || self.primK;
+			self.autoinc = options.autoinc || self.autoinc;
+			self.typeOpt1 = options.typeOpt1 || self.typeOpt1;
+			self.typeOpt2 = options.typeOpt2 || self.typeOpt2;
+			self.FK = options.FK || self.FK;
+		}
 	}
-	this.deleteRow = function (){}
-	this.editRow = function (){}
-	this.moveRow = function (){}
+	let fieldList = [];
+	this.appendField = function(options) {
+		let nfield = new field(options);
+		if(nfield === null) {
+			alert("Не удалось добавить поле");
+			return;
+		}
+		console.log(self, this, fieldList);
+		fieldList.push(nfield);
+		let row = tbody.insertRow(-1);
+		row.insertCell(0).innerHTML = (nfield.primK)? "+" : " ";
+		row.insertCell(1).innerHTML = nfield.name;
+		row.insertCell(2).innerHTML = nfield.type;
+		row.insertCell(3).innerHTML = "  ";
+	}
+	this.deleteRow = function(num) {
+		tbody.removeChild(tbody.rows(num));
+		fieldList.splice(num, 1);
+	}
+	this.updateField = function(num, options) {
+		fieldList[num].editField(options);
+	}
+	this.moveRow = function(numSrc, numDst) {
+		let removed = fieldList.splice(numSrc, 1);
+		let insNum = (numSrc < numDst)? numDst - 1: numDst;
+		fieldList.splice(insNum, 0, removed);
+		DOMtable.deleteRow(numSrc);
+		if (tbody.rows.length < insNum + 1)
+			tbody.appendChild(removed);
+		else
+			tbody.insertBefore(removed, tbody.children[insNum + 1]);
+	}
+	this.getFields = function() {
+		return fieldList.slice();
+	}
+	this.getName = function() {
+		return name;
+	}
+	this.setName = function(tname) {
+		name = tname || name;
+	}
+	this.getConnections = function() {}
 }
 
 let tableList=[];
@@ -94,6 +151,7 @@ function makeDraggableTable(elem) {
 	let shiftX = 0, shiftY = 0;
 	let canvasBox = {};
 	function mouseDown(e) {
+		elem.style.zIndex = 1000;
 		let clickCoords = getCanvasClickCoords(e);
 		shiftX = elem.offsetLeft - clickCoords.left;
 		shiftY = elem.offsetTop - clickCoords.top;
@@ -111,7 +169,43 @@ function makeDraggableTable(elem) {
 		document.onmousemove = null;
 		document.onmouseup = null;
 		document.onselectstart = null;
+		elem.style.zIndex = 0;
 	}
+}
+
+function makeSelectableTable(elem) {
+	elem.onclick = function(e) {
+		let side = document.getElementById("sidebar");
+		let panels = side.getElementsByClassName("panel");
+		let index = null;
+		for (let i = 0; i < tableList.length; i++) {
+			if(tableList[i].DOMtable === elem){
+				index=i;
+				break;
+			}
+		}
+		if (index==null) {
+			console.log(elem, tableList, "Таблица не найдена");
+			return false;
+		}
+		for (let i = 0; i < panels.length; i++) {
+			switch(panels[i].getAttribute("data-content")) {
+				case "table":
+					let text = document.createTextNode(tableList[index].getName());
+					panels[i].appendChild(text);
+					break;
+				case "fields":
+					let fields = tableList[index].getFields();
+					let table = document.createElement("table");
+					for (let j = 0; j < fields.length; j++) {
+						table.insertRow.innerHTML=fields[j];
+					}
+					break;
+				case "connectrions":
+					break;
+			}
+		}
+	} 
 }
 
 function createTable(e) {
@@ -120,24 +214,19 @@ function createTable(e) {
 	let header = table.createTHead();
 	table.tHead.insertRow(0).innerHTML = "<th colspan=\"8\" class=\"theader\">Table "+(tableList.length+1)+"</th>";
 
-	let tFoot = document.createElement('tfoot');
-	let th = document.createElement('th');
-	th.colSpan=8;
-	th.className="tfooter";
-	var btn = document.createElement('button');
-	btn.className="button";
-	btn.innerHTML="Добавить...";
-	th.appendChild(btn);
-	tFoot.insertRow(0).appendChild(th);
-	// tFoot.insertRow(0).innerHTML = "<th colspan=\"8\" class=\"tfooter\"><button class=\"button\">"+
-	// "Добавить...</button></th>";
-	table.appendChild(tFoot);
+	// let tFoot = document.createElement('tfoot');
+	// let th = document.createElement('th');
+	// th.colSpan=8;
+	// th.className="tfooter";
+	// let btn = document.createElement('button');
+	// btn.className="button";
+	// btn.innerHTML="Добавить...";
+	// th.appendChild(btn);
+	// tFoot.insertRow(0).appendChild(th);
+	// table.appendChild(tFoot);
 
-	let tBody = document.createElement('tbody');
-	let initRow = tBody.insertRow(0);
-	initRow.insertCell(0).innerHTML = "Cell 1";
-	initRow.insertCell(1).innerHTML = "Cell 2";
-	table.appendChild(tBody);
+	let tbody = document.createElement('tbody');
+	table.appendChild(tbody);
 
 	let canvasBox = getCanvasBoxOffset();
 	canvas.appendChild(table);
@@ -166,8 +255,13 @@ function createTable(e) {
 
 	}
 	makeDraggableTable(table);
-	let tableM = new tableManager(table);
-	btn.onclick = tableM.appendRow;
+	makeSelectableTable(table);
+	let tableM = new tableManager(table, "Table" + (tableList.length+1));
+	tableM.appendField({
+		name: "lalal",
+		type: "int"
+	})
+	// btn.onclick = tableM.appendRow;
 	tableList.push(tableM);
 }
 
